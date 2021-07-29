afcdfdf2539e229afaa8f15710e4ea9105b0f2b6
DESKTOP-H1GVFHO\zelik <zelik.levit@perkinelmer.com>
2021-07-29 18:13:58 -0400

    Add project files.

---
+++ StockSimulatorClient/MEFContainer.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.Composition.Hosting;
+
+namespace StockSimulatorClient
+{
+	public static class Mef
+	{
+		private static CompositionContainer container;
+
+		public static CompositionContainer Container
+		{
+			get
+			{
+				if (container == null)
+				{
+					var catalog =
+						new DirectoryCatalog(".", "StockSimulator.Component.*");
+
+					container = new CompositionContainer(catalog);					
+				}
+
+				return container;
+			}
+		}
+	}
+}
---
+++ StockSimulatorClient/StockItemsClient.cs
@@ -0,0 +1,19 @@
+﻿using StockSimulator.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockSimulatorClient
+{
+	public class StockItemsClient
+	{
+		public static IStockItemsProvider GetStockItemsProvider()
+		{
+			var provider = Mef.Container.GetExportedValue<IStockItemsProvider>();
+			provider.Start();
+			return provider;
+		}	
+	}
+}
